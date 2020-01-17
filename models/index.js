e771953203c680452aa8438430affd77a6f7dac9
Joaquin Vargas <joaquin.vargas90@gmail.com>
2020-01-16 20:20:47 -0600

    env

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log(env)
 console.log(__dirname)
 const config = require( '../config/config.json')[env];
 console.log(config)
